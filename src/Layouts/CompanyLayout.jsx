5548232321e58bf4ac897c42df90a32e075ba359
BrunoFort <brunomj@hotmail.com>
2026-01-24 16:58:12 -0500

    Update CompanyLayout.jsx

--- src/Layouts/CompanyLayout.jsx
+++ src/Layouts/CompanyLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import { cn } from "../utils";
+import { cn } from "@/lib/utils";
 import {
   LayoutDashboard,
   Users,
