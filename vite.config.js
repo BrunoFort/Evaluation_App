b08e0759a3d9a24f30b32be0108c6bb51cd1c46b
BrunoFort <brunomj@hotmail.com>
2026-01-24 16:42:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-})
+});
