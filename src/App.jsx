5cb1194b0767fd76a2f7463d37752da2ef1f407f
BrunoFort <brunomj@hotmail.com>
2026-01-23 21:49:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import PublicEvaluation from "../Pages/PublicEvaluation";
 import Layout from './components/Layout.jsx';
 import Home from './Pages/Home.jsx';
+
+<Route path="/PublicEvaluation" element={<PublicEvaluation />} />
+
 
 function App() {
   return (
