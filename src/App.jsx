5c3f72f9accea4b63772edbc7e742d3cb7ce5928
BrunoFort <brunomj@hotmail.com>
2026-01-23 23:43:34 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import { Routes, Route } from "react-router-dom";
 
 // Páginas
 import Home from "./Pages/Home.jsx";
-import CompanyLogin from "./Pages/CompanyLogin/CompanyLogin.jsx";
-import CompanyRegistration from "./Pages/CompanyRegistration/CompanyRegistration.jsx";
-import EmployeeLogin from "./Pages/EmployeeLogin/EmployeeLogin.jsx";
+import CompanyLogin from "./Pages/CompanyLogin.jsx";
+import CompanyRegistration from "./Pages/CompanyRegistration.jsx";
+import EmployeeLogin from "./Pages/EmployeeLogin.jsx";
 import PublicEvaluation from "./Pages/PublicEvaluation/PublicEvaluation.jsx";
 
 export default function App() {
   return (
     <Routes>
-      {/* Página inicial */}
       <Route path="/" element={<Home />} />
-
-      {/* Rotas da empresa */}
       <Route path="/CompanyLogin" element={<CompanyLogin />} />
       <Route path="/CompanyRegistration" element={<CompanyRegistration />} />
-
-      {/* Rotas do funcionário */}
       <Route path="/EmployeeLogin" element={<EmployeeLogin />} />
-
-      {/* Página pública de avaliação */}
       <Route path="/PublicEvaluation" element={<PublicEvaluation />} />
     </Routes>
   );
