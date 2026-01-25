60a6bbabfd9464c80d2d93ffca4a1bb1daf30c64
BrunoFort <brunomj@hotmail.com>
2026-01-25 14:13:13 -0500

    Update useUpdateEvaluation.js

--- src/features/evaluations/hooks/useUpdateEvaluation.js
+++ src/features/evaluations/hooks/useUpdateEvaluation.js
@@ -1,8 +1,19 @@
-import { updateEvaluation } from "../api/evaluationsApi";
+import { useState } from "react";
+import { updateEvaluation } from "../evaluationsStore";
 
 export function useUpdateEvaluation() {
+  const [updating, setUpdating] = useState(false);
+
   async function update(id, data) {
-    return updateEvaluation(id, data);
+    setUpdating(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 400));
+
+    updateEvaluation(id, data);
+
+    setUpdating(false);
+    return { success: true };
   }
-  return { update };
+
+  return { update, updating };
 }
