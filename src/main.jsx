9d27eb272df39f23fcf976e53925db079ee09f60
BrunoFort <brunomj@hotmail.com>
2026-01-25 11:06:51 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,13 @@ import App from "./App";
 import "./index.css";
 
 // Importa o AuthProvider
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./features/auth/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
+
