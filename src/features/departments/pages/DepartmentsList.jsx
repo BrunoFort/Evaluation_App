6e14ea1beabc29a88524c819e37b38bc7b3e794d
BrunoFort <brunomj@hotmail.com>
2026-01-24 21:59:53 -0500

    Create DepartmentsList.jsx

---
+++ src/features/departments/pages/DepartmentsList.jsx
@@ -0,0 +1,26 @@
+import { useDepartments } from "../hooks/useDepartments";
+import { useDeleteDepartment } from "../hooks/useDeleteDepartment";
+import { DepartmentTable } from "../components/DepartmentTable";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+export default function DepartmentsList() {
+  const { departments, loading } = useDepartments();
+  const { remove } = useDeleteDepartment();
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="p-6">
+      <div className="flex justify-between mb-4">
+        <h1 className="text-2xl font-bold">Departments</h1>
+
+        <Button asChild>
+          <Link to="/departments/create">New Department</Link>
+        </Button>
+      </div>
+
+      <DepartmentTable departments={departments} onDelete={remove} />
+    </div>
+  );
+}
