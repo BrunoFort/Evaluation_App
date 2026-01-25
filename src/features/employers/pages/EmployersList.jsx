07062c925ae1068e5b69f9ed4fb4ddac03be2b3f
BrunoFort <brunomj@hotmail.com>
2026-01-25 10:20:03 -0500

    Create EmployersList.jsx

---
+++ src/features/employers/pages/EmployersList.jsx
@@ -0,0 +1,26 @@
+import { useEmployers } from "../hooks/useEmployers";
+import { useDeleteEmployer } from "../hooks/useDeleteEmployer";
+import { EmployerTable } from "../components/EmployerTable";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+export default function EmployersList() {
+  const { employers, loading } = useEmployers();
+  const { remove } = useDeleteEmployer();
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="p-6">
+      <div className="flex justify-between mb-4">
+        <h1 className="text-2xl font-bold">Employers</h1>
+
+        <Button asChild>
+          <Link to="/employers/create">New Employer</Link>
+        </Button>
+      </div>
+
+      <EmployerTable employers={employers} onDelete={remove} />
+    </div>
+  );
+}
