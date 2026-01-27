6a7e27c75fc53901cfa407cf8cf5c62bda539530
BrunoFort <brunomj@hotmail.com>
2026-01-26 19:39:20 -0500

    Update EmployeeDashboard.jsx

--- src/Pages/EmployeeDashboard.jsx
+++ src/Pages/EmployeeDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import EmployeeLayout from "@/Layouts/EmployeeLayout";
+import PublicLayout from "../Layouts/PublicLayout";
 import { Link } from "react-router-dom";
 
 export default function EmployeeDashboard() {
