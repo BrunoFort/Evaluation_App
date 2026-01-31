674cd24c2748b3b8a5d2104bbf26af9212192a6b
BrunoFort <brunomj@hotmail.com>
2026-01-31 14:22:48 -0500

    Update collapsable.jsx

--- src/components/ui/collapsable.jsx
+++ src/components/ui/collapsable.jsx
@@ -3,9 +3,16 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
 const Collapsible = CollapsiblePrimitive.Root
+Collapsible.displayName = "Collapsible"
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+CollapsibleTrigger.displayName = "CollapsibleTrigger"
 
 const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+CollapsibleContent.displayName = "CollapsibleContent"
 
-export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+export {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+}
