1a6a57cace33846be8b1e98104acf937ed295bb2
BrunoFort <brunomj@hotmail.com>
2026-01-31 14:37:20 -0500

    Update input-opt.jsx

--- src/components/ui/input-opt.jsx
+++ src/components/ui/input-opt.jsx
@@ -4,13 +4,19 @@ import { Minus } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const InputOTP = React.forwardRef(({ className, containerClassName, ...props }, ref) => (
-  <OTPInput
-    ref={ref}
-    containerClassName={cn("flex items-center gap-2 has-[:disabled]:opacity-50", containerClassName)}
-    className={cn("disabled:cursor-not-allowed", className)}
-    {...props} />
-))
+const InputOTP = React.forwardRef(
+  ({ className, containerClassName, ...props }, ref) => (
+    <OTPInput
+      ref={ref}
+      containerClassName={cn(
+        "flex items-center gap-2 has-[:disabled]:opacity-50",
+        containerClassName
+      )}
+      className={cn("disabled:cursor-not-allowed", className)}
+      {...props}
+    />
+  )
+)
 InputOTP.displayName = "InputOTP"
 
 const InputOTPGroup = React.forwardRef(({ className, ...props }, ref) => (
@@ -23,29 +29,32 @@ const InputOTPSlot = React.forwardRef(({ index, className, ...props }, ref) => {
   const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index]
 
   return (
-    (<div
+    <div
       ref={ref}
       className={cn(
-        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-        isActive && "z-10 ring-1 ring-ring",
+        "relative flex h-9 w-9 items-center justify-center",
+        "border-y border-r border-neutral-300 text-sm transition-all",
+        "first:rounded-l-md first:border-l last:rounded-r-md",
+        isActive && "z-10 ring-2 ring-purple-600",
         className
       )}
-      {...props}>
+      {...props}
+    >
       {char}
+
       {hasFakeCaret && (
-        <div
-          className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+          <div className="h-4 w-px animate-caret-blink bg-neutral-900 duration-1000" />
         </div>
       )}
-    </div>)
-  );
+    </div>
+  )
 })
 InputOTPSlot.displayName = "InputOTPSlot"
 
 const InputOTPSeparator = React.forwardRef(({ ...props }, ref) => (
   <div ref={ref} role="separator" {...props}>
-    <Minus />
+    <Minus className="text-neutral-500" />
   </div>
 ))
 InputOTPSeparator.displayName = "InputOTPSeparator"
