4f6a9b5532043b22956ac6116cb06ec8ebe3379a
BrunoFort <brunomj@hotmail.com>
2026-01-31 12:41:00 -0500

    Delete src/utils/calculateAverage.js

--- src/utils/calculateAverage.js
+++
@@ -1,5 +0,0 @@
-export function calculateAverage(values) {
-  if (!values || values.length === 0) return 0;
-  const sum = values.reduce((acc, v) => acc + Number(v), 0);
-  return Number((sum / values.length).toFixed(2));
-}
