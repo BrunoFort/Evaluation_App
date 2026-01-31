c8acaa177f5b3f0efff151cd9326add6dafb3952
BrunoFort <brunomj@hotmail.com>
2026-01-31 12:45:08 -0500

    Update and rename formatDate.js to formatDate.js

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,5 +1,10 @@
 export function formatDate(dateString) {
   const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    return "";
+  }
+
   return date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
