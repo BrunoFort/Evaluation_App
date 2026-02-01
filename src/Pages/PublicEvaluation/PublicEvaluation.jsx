5f5e6c37ba334e9c99c0b72778a6d36727dbe853
BrunoFort <brunomj@hotmail.com>
2026-02-01 12:39:05 -0500

    Create PublicEvaluation.jsx

---
+++ src/Pages/PublicEvaluation/PublicEvaluation.jsx
@@ -0,0 +1,8 @@
+export default function PublicEvaluation() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Public Evaluation</h1>
+      <p>Página restaurada temporariamente.</p>
+    </div>
+  );
+}
