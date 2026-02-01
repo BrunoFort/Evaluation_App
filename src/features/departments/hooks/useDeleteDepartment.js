84aede035f2a0fc6630f9019e238fb355d8b8049
BrunoFort <brunomj@hotmail.com>
2026-01-31 20:38:33 -0500

    Update useDeleteDepartment.js

--- src/features/departments/hooks/useDeleteDepartment.js
+++ src/features/departments/hooks/useDeleteDepartment.js
@@ -2,7 +2,13 @@ import { deleteDepartment } from "../api/departmentsApi";
 
 export function useDeleteDepartment() {
   async function remove(id) {
-    return deleteDepartment(id);
+    try {
+      return await deleteDepartment(id);
+    } catch (err) {
+      console.error("Failed to delete department:", err);
+      throw err;
+    }
   }
+
   return { remove };
 }
