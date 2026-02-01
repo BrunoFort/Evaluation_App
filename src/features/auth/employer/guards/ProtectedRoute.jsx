41cdf28a969674a2d92276af7ece56938f35d066
BrunoFort <brunomj@hotmail.com>
2026-01-31 20:23:55 -0500

    Update and rename src/features/auth/ProtectedRoute.jsx to src/features/auth/employer/guards/ProtectedRoute.jsx

--- src/features/auth/employer/guards/ProtectedRoute.jsx
+++ src/features/auth/employer/guards/ProtectedRoute.jsx
@@ -1,10 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { useEmployerAuth } from "./employer/useEmployerAuth";
+import { useEmployerAuth } from "../hooks/useEmployerAuth";
 
 export default function ProtectedRoute({ children }) {
   const { employer } = useEmployerAuth();
 
-  // Se não estiver logado, redireciona para login
   if (!employer) {
     return <Navigate to="/employer/login" replace />;
   }
