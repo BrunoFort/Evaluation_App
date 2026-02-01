3a4a6f16046071bac6b4eb31f8866707b0154bd4
BrunoFort <brunomj@hotmail.com>
2026-02-01 11:48:59 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="text-neutral-800 p-6">
+      <h1 className="text-3xl font-bold">ProRef</h1>
+      <p>App is running.</p>
+    </div>
+  );
+}
