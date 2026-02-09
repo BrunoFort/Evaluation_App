6c727dd4b1956312a7eb0251682d58d3c72688c0
BrunoFort <brunomj@hotmail.com>
2026-02-09 17:27:54 -0500

    Testing back icons

--- src/components/ui/IconBack.jsx
+++ src/components/ui/IconBack.jsx
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 
 export default function IconBack({ className = "" }) {
   return (
     <span className={`relative inline-flex items-center justify-center ${className}`}>
-      <FontAwesomeIcon icon={faArrowLeft} className="h-4 w-4" />
+      <FontAwesomeIcon icon={faArrowLeftLong} className="h-4 w-4" />
       <FontAwesomeIcon
         icon={faStar}
-        className="absolute -top-1 -right-1 h-2.5 w-2.5 opacity-60"
+        className="absolute -top-1 -right-2 h-2.5 w-2.5 opacity-50"
       />
     </span>
   );
