6a6da1bb6440896594cebb76a52f28ecb921cb7b
BrunoFort <brunomj@hotmail.com>
2026-02-20 15:30:21 -0500

    Still fixing layouts

--- src/layouts/EmployerDashboardLayout.jsx
+++ src/layouts/EmployerDashboardLayout.jsx
@@ -76,10 +76,10 @@ export default function EmployerDashboardLayout({ children }) {
     <div className="flex min-h-screen bg-neutral-50">
 
       {/* SIDEBAR */}
-      <aside className="w-64 bg-gradient-to-b from-neutral-900 to-neutral-800 text-white flex flex-col py-6 px-6">
+      <aside className="w-64 bg-gradient-to-b from-neutral-900 to-neutral-800 text-white flex flex-col pt-2 pb-4 px-6">
 
         {/* Logo */}
-        <div className="flex items-center justify-center mb-2">
+        <div className="flex items-center justify-center mb-1">
           <img
             src={SHIME_LOGO_BLACK}
             alt="Shime Logo"
