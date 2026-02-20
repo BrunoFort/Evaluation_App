4155f62b349fe3345f9a918cf9b349fa734e08d8
BrunoFort <brunomj@hotmail.com>
2026-02-20 16:21:34 -0500

    testing w-30 h-30

--- src/layouts/EmployeeDashboardLayout.jsx
+++ src/layouts/EmployeeDashboardLayout.jsx
@@ -28,7 +28,7 @@ export default function EmployeeDashboardLayout({ children }) {
           <img
             src={SHIME_LOGO_BLACK}
             alt="Shime Logo"
-            className="w-72 h-72 object-contain"
+            className="w-30 h-30 object-contain"
           />
         </div>
 
